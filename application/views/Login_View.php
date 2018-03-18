54c0e1cd9cb93fbf874613beb9849f8167a0b5b6
Nam Doan <31881753+QNam@users.noreply.github.com>
2018-03-19 01:52:47 +0700

    Add files via upload
    QNam đã edit

--- application/views/Login_View.php
+++ application/views/Login_View.php
@@ -1,25 +1,16 @@
+	<?php var_dump($_SESSION)?>
+<div class="wrapper">
+	<form class="form-signin" action="" method="POST">       
+		<h2 class="form-signin-heading text-center">Đăng nhập</h2>
 
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
-			
-		</div>
-		<div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
-			<form action="login" method="POST" class="form-group">
-				<label>Mã SV: </label>
-				<input type="text" name="id" class="form-control" value="<?php echo set_value('id')?>">
-				<div class="text-danger"><?php echo form_error('id')?></div>
+		<input type="text" class="form-control" name="id" placeholder="Mã sinh viên" required="" autofocus="" value="<?php echo set_value('id')?>"/>
+		<div class="text-danger"><?php echo form_error('id')?></div>
 
-				<label>Password: </label>
-				<input type="text" name="password" class="form-control" value="<?php echo set_value('password')?>">
-				<div class="text-danger"><?php echo form_error('password')?></div>
+		<input type="password" class="form-control" name="password" placeholder="Mật khẩu" required="" autofocus="" value="<?php echo set_value('password')?>" /> 
+		<div class="text-danger"><?php echo form_error('password')?></div>     
 
-				<button class="btn btn-info" type="submit" name="login">Đăng nhập</button>
-			</form>
-		</div>
-		<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
-			
-		</div>
-	</div>
+		<button class="btn btn-lg btn-primary btn-block" type="submit">Đăng nhập</button>   
+	</form>
 </div>
 
+		
