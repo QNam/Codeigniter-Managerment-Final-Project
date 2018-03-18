94e4b1496718c01a3696239705d6e8fb20bc5688
Nam Doan <31881753+QNam@users.noreply.github.com>
2018-03-19 01:54:15 +0700

    Add files via upload

---
+++ application/views/template/header.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+		<title>UTC</title>
+	  <meta charset="utf-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	  <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+	  <style type="text/css">
+
+		.wrapper {
+		  margin-top: 80px;
+		  margin-bottom: 80px;
+		}
+
+		.form-signin {
+		  max-width: 380px;
+		  padding: 15px 35px 45px;
+		  margin: 0 auto;
+		  background-color: #fff;
+		  border: 1px solid rgba(0, 0, 0, 0.1);
+		}
+		.form-signin .form-signin-heading,
+		.form-signin .checkbox {
+		  margin-bottom: 30px;
+		}
+		.form-signin .checkbox {
+		  font-weight: normal;
+		}
+		.form-signin .form-control {
+		  position: relative;
+		  font-size: 16px;
+		  height: auto;
+		  padding: 10px;
+		  -webkit-box-sizing: border-box;
+		  -moz-box-sizing: border-box;
+		  box-sizing: border-box;
+		}
+		.form-signin .form-control:focus {
+		  z-index: 2;
+		}
+		.form-signin input[type="text"] {
+		  margin-bottom: -1px;
+		  border-bottom-left-radius: 0;
+		  border-bottom-right-radius: 0;
+		}
+		.form-signin input[type="password"] {
+		  margin-bottom: 20px;
+		  border-top-left-radius: 0;
+		  border-top-right-radius: 0;
+		}
+
+	</style>
+</head>
+<body>
+	  
+
+	  
