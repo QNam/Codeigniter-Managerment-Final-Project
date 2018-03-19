02991e224f623760045b534cc524427a2dc53aa6
BuiThanhNam1998 <37493609+BuiThanhNam1998@users.noreply.github.com>
2018-03-19 20:22:26 +0700

    Add files via upload

---
+++ application/views/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>A Pen by  Zoltan Kohalmy</title>
+  
+  
+  
+      <link rel="stylesheet" href="css/style.css">
+
+  
+</head>
+
+<body>
+
+  
+<form>
+  <header>Register</header>
+  <h3> Register </h3>
+  <label>ID studen <span></span></label>
+  <input type="text" class="form-control" name="IDstuden" required="" autofocus="" />
+  <label>Huong chuyen sau</label><br>
+  <select name="Huong chuyen sau" class="form-control">
+    <option class="opt" value="1">Lap trinh web</option>
+    <option class="opt" value="2">Androi</option>
+    <option class="opt" value="3">IOS</option>
+  </select >
+  <label>Giao vien dang ki</label><br>
+  <select name="Giao vien dang ki" class="form-control">
+    <option class="opt" value="1">Hoang Van Thong</option>
+    <option class="opt" value="2">Nguyen Trong Phuc</option>
+    <option class="opt" value="3">Nhuyen Hieu Cuong</option>
+  </select>
+  <label>De cuong noi dung, cong nghe su dung</label><br>
+  <textarea rows="3" cols="60"></textarea> 
+  <label>Noi dung ke hoach</label><br>
+  <textarea rows="3" cols="60"></textarea> 
+  <label>Thoi gian thuc hien ke hoach</label><br>
+  <textarea rows="3" cols="60"></textarea> 
+  <button type="submit ">Register</button>
+</form>
+  
+  
+
+</body>
+
+</html>
